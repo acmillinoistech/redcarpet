0586333bf86373192b9c1fdfdc78a128679dafdc
Vinesh Kannan <vinesh@omnipointment.com>
2019-02-11 01:07:29 -0600

    Add Bulma for styling

--- src/App.js
+++ src/App.js
@@ -20,9 +20,19 @@ class App extends Component {
     console.log(this.props.tiles)
     return (
       <div className="App">
-        <h1>Illinois Tech Celebrities</h1>
-        <p>Here are some you might like:</p>
-        <ul>{tiles}</ul>
+        <section className="hero is-info is-bold">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">Illinois Tech Celebrities</h1>
+            </div>
+          </div>
+        </section>
+        <section className="section">        
+        <div className="container content">
+            <p>Here are some you might like:</p>
+            <ul>{tiles}</ul>
+        </div>
+        </section>
       </div>
     );
   }
